df4359fa30231724f563e548d06fb87c5443a860
Brenon Araujor <brenon.araujo@nastek.com.br>
2017-08-16 13:41:38 -0400

    next page implementado e alteração nos incos de peoples

--- src/app/peoples/components/peoples-list.vue
+++ src/app/peoples/components/peoples-list.vue
@@ -2,14 +2,16 @@
   <div class="bloco">
     <ul v-if="peoples && peoples.length">
       <li v-for="people of peoples">
+           <i class="Large material-icons">account_box</i>
         <p><strong>{{people.name}}</strong></p>
       </li>
     </ul>
+    <button type="submit" v-on:click="nextPage" >Next</button>
   </div>
 </template>
 
 <script>
-import { getPeoples } from '../services'
+import { getPeoples, getNextPeoples } from '../services'
 
 export default {
   data: () => ({
@@ -24,6 +26,16 @@ export default {
       this.previous = data.previous
       this.peoples = data.results
     })
+  },
+  methods: {
+    nextPage: function (event) {
+      console.log(this.next)
+      getNextPeoples(this.next).then(data => {
+        this.peoples = data.results
+        this.next = data.next
+        this.previous = data.previous
+      })
+    }
   }
 }
 </script>
@@ -45,4 +57,32 @@ button {
   display: inline-block;
   margin: 10px 5px;
 }
+
+/* fallback */
+@font-face {
+  font-family: 'Material Icons';
+  font-style: normal;
+  font-weight: 400;
+  src: url(https://fonts.gstatic.com/s/materialicons/v28/2fcrYFNaTjcS6g4U3t-Y5ZjZjT5FdEJ140U2DJYC3mY.woff2) format('woff2');
+}
+
+.material-icons {
+  font-family: 'Material Icons';
+  font-weight: normal;
+  color: #dbd553;
+  font-style: normal;
+  font-size: 180px;
+  line-height: 1;
+  letter-spacing: normal;
+  text-transform: none;
+  display: inline-block;
+  white-space: nowrap;
+  word-wrap: normal;
+  direction: ltr;
+  -moz-font-feature-settings: 'liga';
+  -moz-osx-font-smoothing: grayscale;
+}
+
+
 </style>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
--- src/app/peoples/services.js
+++ src/app/peoples/services.js
@@ -4,3 +4,8 @@ export const getPeoples = () => {
   return http.get('https://swapi.co/api/people')
   .then(response => response.data)
 }
+
+export const getNextPeoples = (page) => {
+  return http.get(page)
+  .then(response => response.data)
+}
